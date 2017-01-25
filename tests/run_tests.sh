ce3d8c6a1d2829feab5d8bcaf1b10e93d2f58105
Daniel Schneller <ds@danielschneller.de>
2017-01-25 17:52:59 +0100

    Don't expose secret via --diff

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ansible-playbook test.yml -i inventory --extra-vars="S_INSTANA_AGENT_KEY=${S_INSTANA_AGENT_KEY}"  --diff $@
+ansible-playbook test.yml -i inventory --extra-vars="S_INSTANA_AGENT_KEY=${S_INSTANA_AGENT_KEY}"  $@
 
